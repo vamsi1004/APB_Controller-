17b8eb7bda073f852132c5f5e637044e1edf0aa1
vamsi1004 <100430503+vamsi1004@users.noreply.github.com>
2025-09-22 13:32:09 +0530

    Create APB_sequencer.sv

---
+++ TestBench/APB_sequencer.sv
@@ -0,0 +1,11 @@
+`include"defines.svh" 
+
+class apb_sequencer extends uvm_sequencer #(apb_seq_item);
+  
+  `uvm_component_utils(apb_sequencer)
+  
+  function new(string name = "apb_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
